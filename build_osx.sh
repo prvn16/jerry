3aad34f49d7585303738f0fb8ec9384b6bdbae1b
asdfjkl <asdfjkl@users.noreply.github.com>
2015-02-28 22:57:27 +0100

    this is the homepage - dir
    Former-commit-id: 41d1abe8606fb596c344225cd31362ee6dd1b7c9

--- build_osx.sh
+++
@@ -1,4 +0,0 @@
-python3 setup.py py2app --includes "sip"
-cp -R res dist/Jerry.app/Contents/Resources/
-cp -R books dist/Jerry.app/Contents/Resources/
-cp -R engine dist/Jerry.app/Contents/Resources/
--- util/messages.py
+++
@@ -1,7 +0,0 @@
-from PyQt4.QtGui import QMessageBox
-
-def display_mbox(title,content):
-    msgBox = QMessageBox()
-    msgBox.setText(title)
-    msgBox.setInformativeText(content)
-    msgBox.exec_()
